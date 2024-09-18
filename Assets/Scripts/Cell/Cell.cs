8ac80cec169475ffe2e07b0e0b431cc6a825fb6e
Mohammad Reza <97195961+mohammadrezahamedi@users.noreply.github.com>
2024-09-18 16:30:13 +0330

    Update Cell.cs

--- Assets/Scripts/Cell/Cell.cs
+++ Assets/Scripts/Cell/Cell.cs
@@ -4,10 +4,10 @@ namespace RealityUnit
 {
     public class Cell : BaseCell
     {
-        // Implement the abstract method, potentially adding specific behavior for this type of cell
+     
         public override void SpecialBehavior()
         {
-            // Add any specialized behavior for this cell here
+      
             Debug.Log("Special behavior for the Cell class.");
         }
     }
